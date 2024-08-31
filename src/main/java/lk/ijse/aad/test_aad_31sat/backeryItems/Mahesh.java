abbae184e543f0ed125d9f8ffa57d97a4af420ee
ChathuminiBandara <chathubandara500@gmail.com>
2024-08-31 10:45:12 +0530

    user

--- src/main/java/lk/ijse/aad/test_aad_31sat/backeryItems/Mahesh.java
+++ src/main/java/lk/ijse/aad/test_aad_31sat/backeryItems/Mahesh.java
@@ -1,2 +1,14 @@
-package lk.ijse.aad.test_aad_31sat.backeryItems;public class Mahesh {
+package lk.ijse.aad.test_aad_31sat.backeryItems;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Mahesh {
+    public Mahesh(){
+
+    }
+
+    public void preference(){
+        System.out.println("Mahesh's Current preference !");
+    }
 }
