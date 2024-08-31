b0eb7a6e8f7a18e9b4676c30c966afe375ccf7e4
ChathuminiBandara <chathubandara500@gmail.com>
2024-08-31 10:44:53 +0530

    config class

--- src/main/java/lk/ijse/aad/test_aad_31sat/config/config.java
+++ src/main/java/lk/ijse/aad/test_aad_31sat/config/config.java
@@ -1,2 +1,12 @@
-package lk.ijse.aad.test_aad_31sat.config;public class config {
+package lk.ijse.aad.test_aad_31sat.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+@Component
+@ComponentScan(basePackages = "lk.ijse.aad.test_aad_31sat")
+public class config {
+
+
+
 }
